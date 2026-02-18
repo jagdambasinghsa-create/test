a65c9e150d911f0b06f4cfbc4a994ec66f7ec960
jagdambasinghsa-create <jagdambasinghsa@gmail.com>
2026-02-18 13:25:56 +0530

    Delete ping.js

--- ping.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-setInterval(() => {
-  http.get('https://firebasepassword.onrender.com', (res) => {
-    console.log(`Pinged server: ${res.statusCode}`);
-  }).on('error', (err) => {
-    console.log('Error pinging server:', err.message);
-  });
-}, 2 * 60 * 1000); // har 4 minute ping
